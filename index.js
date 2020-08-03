2bc67902590ab310c942d58fca107c8a7d22f285
muddybootscode <firsttogo98@gmail.com>
2020-08-03 15:43:53 -0400

    Feature: Added skelton code for api

---
+++ index.js
@@ -0,0 +1,59 @@
+import { typeDefs } from './graphql-schema'
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import neo4j from 'neo4j-driver'
+import { makeAugmentedSchema } from 'neo4j-graphql-js'
+import dotenv from 'dotenv'
+
+// set environment variables from .env
+dotenv.config()
+
+const app = express()
+
+/*
+ * Create a Neo4j driver instance to connect to the database
+ * using credentials specified as environment variables
+ * with fallback to defaults
+ */
+
+const driver = neo4j.driver(
+  process.env.NEO4J_URI || 'bolt://localhost:7687',
+  neo4j.auth.basic(
+    process.env.NEO4J_USER || 'neo4j',
+    process.env.NEO4J_PASSWORD || 'neo4j'
+  ),
+  {
+    encrypted: process.env.NEO4J_ENCRYPTED ? 'ENCRYPTION_ON' : 'ENCRYPTION_OFF',
+  }
+)
+
+const augmentedSchema = makeAugmentedSchema({
+  typeDefs,
+})
+/*
+ * Create a new ApolloServer instance, serving the GraphQL schema
+ * created using makeAugmentedSchema above and injecting the Neo4j driver
+ * instance into the context object so it is available in the
+ * generated resolvers to connect to the database.
+ */
+const server = new ApolloServer({
+  context: { driver, neo4jDatabase: process.env.NEO4J_DATABASE },
+  schema: augmentedSchema,
+  introspection: true,
+  playground: true,
+})
+
+// Specify host, port and path for GraphQL endpoint
+const port = process.env.GRAPHQL_SERVER_PORT || 4001
+const path = process.env.GRAPHQL_SERVER_PATH || '/graphql'
+const host = process.env.GRAPHQL_SERVER_HOST || '0.0.0.0'
+
+/*
+ * Optionally, apply Express middleware for authentication, etc
+ * This also also allows us to specify a path for the GraphQL endpoint
+ */
+server.applyMiddleware({ app, path })
+
+app.listen({ host, port, path }, () => {
+  console.log(`GraphQL server ready at http://${host}:${port}${path}`)
+})
