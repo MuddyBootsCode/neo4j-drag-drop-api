6e03d84bf57800b3439aa85d570315aef7fb16de
muddybootscode <firsttogo98@gmail.com>
2020-09-23 09:04:57 -0400

    playing with directives

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ export class SubscribeDirective extends SchemaDirectiveVisitor {
   }
 
   visitObject(object) {
-    console.log(object, ' from sub')
     return super.visitObject(object);
   }
 }
@@ -47,70 +46,22 @@ export class PublishDirective extends SchemaDirectiveVisitor {
 
   visitObject(object) {
     const { to } = this.args;
-    console.log(to, this.args, ' from the top')
     const fields = object.getFields();
     Object.keys(fields).forEach((fieldName) => {
       const field = fields[fieldName];
-      // console.log(field)
       const next = field.resolve;
 
       field.resolve = (result, args, context, info) => {
-        console.log(args, 'args')
-        console.log(result, ' Result')
         pubsub.publish(to, {[to]: result})
-        // console.log(args)
-        // console.log(context)
-        // console.log(info)
         return next(result, args, context, info);
       }
     })
-  }
-}
-
-export class IsPublishedDirective extends SchemaDirectiveVisitor {
-  static getDirectiveDeclaration(directiveName, schema) {
-    return new GraphQLDirective({
-      name: 'isPublished',
-      locations: [DirectiveLocation.FIELD_DEFINITION, DirectiveLocation.OBJECT]
-    });
-  }
-
-  visitObject(obj) {
-    const fields = obj.getFields();
-    console.log(this.args, ' upper Args')
-
-    Object.keys(fields).forEach((fieldName) => {
-      const field = fields[fieldName];
-      const next = field.resolve;
-
-      field.resolve = (result, args, context, info) => {
-        // console.log(args, ' Args')
-        console.log(context.req.body, ' request body')
-        console.log(result, ' Result')
-        // console.log(info, ' info')
-        return next(result, args, context, info);
-      }
-    })
-  }
-
-  visitFieldDefinition(field) {
-    const next = field.resolve;
-    console.log(field.astNode.type);
-
-    field.resolve = function(result, args, context, info) {
-      // console.log(result);
-      // console.log(args);
-      // console.log(context);
-      // console.log(info);
-      return next(result, args, context, info);
-    };
   }
 }
 
 const augmentedSchema = makeAugmentedSchema({
   typeDefs,
   schemaDirectives: {
-    isPublished: IsPublishedDirective,
     subscribe: SubscribeDirective,
     publish: PublishDirective
 
@@ -121,12 +72,7 @@ const augmentedSchema = makeAugmentedSchema({
     }
   }
 })
-/*
- * Create a new ApolloServer instance, serving the GraphQL schema
- * created using makeAugmentedSchema above and injecting the Neo4j driver
- * instance into the context object so it is available in the
- * generated resolvers to connect to the database.
- */
+
 const server = new ApolloServer({
   context: ({ req }) => {
     return { req, driver, neo4jDatabase: process.env.NEO4J_DATABASE }
